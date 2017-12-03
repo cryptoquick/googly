ff23c06ac683b3b052da7c27a28f94407f43d31f
Hunter Trujillo <cryptoquick@gmail.com>
2017-12-02 20:48:05 -0700

    Prettier.

--- googly.js
+++ googly.js
@@ -3,7 +3,7 @@ var touchEvents = {}
 var ax = 1
 var ay = 1
 
-function touching () {
+function touching() {
   var vector = ax / ay
   var touches = []
 
@@ -14,7 +14,7 @@ function touching () {
         x: touch.pageX - 100,
         y: touch.pageY - 100,
         gx: touch.pageX - 50 + ax * 5,
-        gy: touch.pageY - 50 - ay * 5
+        gy: touch.pageY - 50 - ay * 5,
       })
     }
   }
@@ -23,27 +23,35 @@ function touching () {
 
   for (var i = 0; i < touches.length; i++) {
     var t = touches[i]
-    touchElements.push("<div class=\"pip\" style=\"left: " + t.x + "px; top: " + t.y + "px;\"></div>")
-    touchElements.push("<div class=\"eye\" style=\"left: " + t.gx + "px; top: " + t.gy + "px;\"></div>")
+    touchElements.push(
+      '<div class="pip" style="left: ' + t.x + 'px; top: ' + t.y + 'px;"></div>'
+    )
+    touchElements.push(
+      '<div class="eye" style="left: ' +
+        t.gx +
+        'px; top: ' +
+        t.gy +
+        'px;"></div>'
+    )
   }
 
   toucher.innerHTML = touchElements.join('\n')
 }
 
-toucher.ontouchstart = function (evt) {
+toucher.ontouchstart = function(evt) {
   touchEvents = evt.touches
   evt.preventDefault()
   return false
 }
 
-toucher.onmousedown = function (evt) {
+toucher.onmousedown = function(evt) {
   touchEvents = [{pageX: evt.pageX, pageY: evt.pageY}]
   touching()
   evt.preventDefault()
   return false
 }
 
-window.ondevicemotion = function (evt) {
+window.ondevicemotion = function(evt) {
   ax = evt.accelerationIncludingGravity.x || 1
   ay = evt.accelerationIncludingGravity.y || 1
   touching()
